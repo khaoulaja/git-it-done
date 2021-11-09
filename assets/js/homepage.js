1c2814821574fc7deef4bf99ce18f0edf6097c63
khaoula <khaoulajabour4@gmail.com>
2021-11-09 03:30:26 -0500

    add single repo html file

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -41,6 +41,20 @@ var displayRepos = function(repos, searchTerm){
 
         //append container to the DOM
         repoContainerEl.appendChild(repoEl);
+
+        //create status element
+        var statusEl = document.createElement("span");
+        statusEl.classList ="flex-row align-center";
+        
+        //check if current repo has issues or not 
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML =  "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        }
+        else{
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+        //append to container
+        repoEl.appendChild(statusEl);
     }
 }
 
