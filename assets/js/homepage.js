0fdad2925c57c4d9aeb986aa285cd4c834306e7d
khaoula <khaoulajabour4@gmail.com>
2021-11-08 20:41:42 -0500

    recover html code

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,18 @@
-function getUserRepos(){
-    fetch("https://api.github.com/users/octocat/repos");
+function getUserRepos(user){
+    var apiUrl = "https://api.github.com/users/"+user+"/repos";
+   var response = fetch(apiUrl).then(function(response){
+       response.json().then(function(data){
+            for (var i = 0; i < data.length; i++) {
+               console.log(data[i].html_url);
+        // Set the text of the list element to the JSON response's .html_url property
+        
+
+        
+      }
+          
+       });
+   });
+   console.log("outside");
 }
-getUserRepos();
+
+getUserRepos("Twitter");
